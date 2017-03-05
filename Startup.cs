98f23e83b834f91ded7be5ef8ca1847c6026b1c9
Eric Sowell <eric.sowell@gmail.com>
2017-03-05 15:55:53 -0600

    error page

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder; //For IApplicationBuilder
 using Microsoft.AspNetCore.Http;    //For Response.WriteAsync
 using Microsoft.Extensions.DependencyInjection; //For IServiceCollection
 using Microsoft.Extensions.Logging; //For ILoggerFactory
+using Microsoft.AspNetCore.Hosting; //To get access to the hosting environment
 
 public class Startup
 {
@@ -12,9 +13,14 @@ public class Startup
     }
 
     //Configure your middleware pipeline
-    public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+    public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment env)
     {
         loggerFactory.AddConsole();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
 
         app.UseMiddleware<ComeAtMeBroMiddleware>();
         app.UseStaticFiles();
