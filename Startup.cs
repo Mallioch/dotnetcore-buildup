d8c84cb59130f2dee7133919a60a72c62d3f36f9
Eric Sowell <eric.sowell@gmail.com>
2017-03-04 23:24:34 -0600

    Code for buildup - middleware explained

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder; //For IApplicationBuilder
 using Microsoft.AspNetCore.Http;    //For Response.WriteAsync
 using Microsoft.Extensions.DependencyInjection; //For IServiceCollection
+using Microsoft.Extensions.Logging; //For ILoggerFactory
 
 public class Startup
 {
@@ -11,12 +12,17 @@ public class Startup
     }
 
     //Configure your middleware pipeline
-    public void Configure(IApplicationBuilder app)
+    public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
     {
+        loggerFactory.AddConsole();
+
+        app.UseMiddleware<ComeAtMeBroMiddleware>();
         app.UseStaticFiles();
 
         app.Run(async (context) =>
         {
+            var logger = loggerFactory.CreateLogger("Pipeline End");
+            logger.LogInformation("The end of the middleware pipeline");
             await context.Response.WriteAsync("Hello World!");
         });
     }
